fda101e00481c7fb303ac132694e2f9c17d4d1b1
Stanislav (Stas) Katkov <github@skatkov.com>
2024-09-26 03:05:26 +0200

    Use external executable wrapper for Tailwind CSS

--- jekyll-tailwind-cli.gemspec
+++ jekyll-tailwind-cli.gemspec
@@ -31,6 +31,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "jekyll", ">= 3.0", "< 5.0"
+  spec.add_dependency "tailwindcss-ruby"
   spec.add_development_dependency "rake"
 
   # For more information and examples about making a new gem, check out our
--- lib/jekyll-tailwind-cli.rb
+++ lib/jekyll-tailwind-cli.rb
@@ -1,21 +1,40 @@
 # frozen_string_literal: true
 
 require_relative "jekyll-tailwind-cli/version"
-require_relative "jekyll-tailwind-cli/installer"
 
 require "jekyll"
+require "tailwindcss/ruby"
 
-def tailwind(site)
-  Jekyll::Tailwind::Installer.new(
-    version: site.config.dig("tailwind", "version"),
-    config_path: site.config.dig("tailwind", "config_path")
-  )
-end
 
-Jekyll::Hooks.register [:site], :post_read do |site|
-  tailwind(site).check_install
+module Jekyll
+  class Tailwind
+    def self.root
+      File.dirname(__dir__)
+    end
+
+    def self.build
+      Jekyll.logger.info 'Tailwind:', 'Rebuilt _site/assets/css/app.css'
+      compile_command
+    end
+
+    def self.compile_command(debug: false, **kwargs)
+      command = [
+                Tailwindcss::Ruby.executable(**kwargs),
+                "--input", Jekyll::Tailwind.root.join("assets/css/app.css").to_s,
+                "--output", Jekyll::Tailwind.root.join("_site/assets/css/app.css").to_s,
+                "--config", Jekyll::Tailwind.root.join(@config_path).to_s,
+              ]
+
+      command << "--minify" unless debug
+
+      postcss_path = Rails.root.join("postcss.config.js")
+      command += ["--postcss", postcss_path.to_s] if File.exist?(postcss_path)
+
+      command
+    end
+  end
 end
 
 Jekyll::Hooks.register [:site], :post_write do |site|
-  tailwind(site).build
+  Jekyll::Tailwind.build
 end
