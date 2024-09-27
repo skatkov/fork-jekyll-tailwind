5bdbcd7a80e2d93a0d9e46309c9b960da73275a5
Stanislav (Stas) Katkov <contact@skatkov.com>
2024-09-27 14:11:02 +0200

    Update jekyll-tailwind-cli.rb

--- lib/jekyll-tailwind-cli.rb
+++ lib/jekyll-tailwind-cli.rb
@@ -14,14 +14,14 @@ module Jekyll
     def self.compile(debug: false, **kwargs)
       command = [
                 Tailwindcss::Ruby.executable(**kwargs),
-                "--input", root.join("assets/css/app.css").to_s,
-                "--output", root.join("_site/assets/css/app.css").to_s,
-                "--config", root.join(@config_path).to_s,
+                "--input", Jekyll::Tailwind.root.join("assets/css/app.css").to_s,
+                "--output", Jekyll::Tailwind.root.join("_site/assets/css/app.css").to_s,
+                "--config", Jekyll::Tailwind.root.join(@config_path).to_s,
               ]
 
       command << "--minify" unless debug
 
-      postcss_path = Rails.root.join("postcss.config.js")
+      postcss_path = Jekyll::Tailwind.root.join("postcss.config.js")
       command += ["--postcss", postcss_path.to_s] if File.exist?(postcss_path)
 
       command
